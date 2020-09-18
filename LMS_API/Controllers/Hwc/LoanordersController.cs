c77ef8cd226c883b4ffabf231540b259d0e7787d
韩文春 <@DESKTOP-HRSQVH5>
2020-09-18 19:06:03 +0800

    hwc

--- LMS_API/Controllers/Hwc/LoanordersController.cs
+++ LMS_API/Controllers/Hwc/LoanordersController.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AD_LMS.Models;
+using LMS_API.Models.WzbModels;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LMS_API.Controllers.Hwc
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]/")]
+    [EnableCors("cors")]
     [ApiController]
+    //https://localhost:44341/api/Loanorders/GetDing
     public class LoanordersController : ControllerBase
     {
-
+        public LMScontext db;
+        public LoanordersController(LMScontext db) { this.db = db; }
+        /// <summary>
+        ///带放款订单
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LMS_Ding>>> GetWareHouses()
+        {
+            return await db.LMS_Ding.ToListAsync();
+        }
     }
 }

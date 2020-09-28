cfa6a360fbcfce0e02c086da759721e2741afae2
王则波 <@DESKTOP-HEGK1L8>
2020-09-28 14:41:51 +0800

    暗室逢灯

---
+++ LMS_API/Models/WzbModels/Rizhi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LMS_API.Models.WzbModels
+{
+    public class Rizhi
+    {
+        public int RId { get; set; }
+        public string RName { get; set; }
+        public string RNei { get; set; }
+        public DateTime RTime { get; set; }
+        public string RFangfa { get; set; }
+    }
+}

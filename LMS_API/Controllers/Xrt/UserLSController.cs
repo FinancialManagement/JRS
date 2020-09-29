aeb13aa54ee69928d9c45a2f0ac01ab668d2c469
徐壬霆 <@DESKTOP-DU2URGK>
2020-09-29 09:53:06 +0800

    111

--- LMS_API/Controllers/Xrt/UserLSController.cs
+++ LMS_API/Controllers/Xrt/UserLSController.cs
@@ -7,6 +7,7 @@ using AD_LMS.Models;
 using AD_LMS.Models.WzbModels;
 using Dapper;
 using LMS_API.Models;
+using LMS_API.Models.Hwc.Product;
 using LMS_API.Models.WzbModels;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
@@ -28,6 +29,7 @@ namespace LMS_API.Controllers.Xrt
 
         public LMScontext db;
         public UserLSController(LMScontext db) { this.db = db; }
+        #region 用户登录注册
         /// <summary>
         /// 用户登录
         /// </summary>
@@ -38,7 +40,7 @@ namespace LMS_API.Controllers.Xrt
         [HttpGet]
         public async Task<ActionResult<int>> Login(string UName, string UPwd)
         {
-            var list = db.UserInfos.Where(s => s.UName.Equals(UName) && s.UPwd.Equals(UPwd)).CountAsync();
+            var list = db.LMS_Client.Where(s => s.SName.Equals(UName) && s.SMm.Equals(UPwd)).CountAsync();
             return await list;
         }
         /// <summary>
@@ -52,7 +54,7 @@ namespace LMS_API.Controllers.Xrt
         public async Task<ActionResult<int>> Regin(string s)
         {
             UserInfos user = JsonConvert.DeserializeObject<UserInfos>(s);
-            if (user==null)
+            if (user == null)
             {
                 return 0;
             }
@@ -61,8 +63,10 @@ namespace LMS_API.Controllers.Xrt
                 db.UserInfos.Add(user);
                 return await db.SaveChangesAsync();
             }
-            
+
         }
+        #endregion
+
         #region 客户申请
         /// <summary>
         /// 客户申请贷款
@@ -126,6 +130,7 @@ namespace LMS_API.Controllers.Xrt
         }
 
         #endregion
+
         #region 下拉
         /// <summary>
         /// 民族下拉
@@ -158,6 +163,117 @@ namespace LMS_API.Controllers.Xrt
             return sql;
         }
         #endregion
+
+        [HttpGet]
+        [Route("ShowDing")]
+        public Models.Hwc.Page ShowDing(string name = "", int PageSize = 3, int PageCurrent = 1)
+        {
+            using (IDbConnection db = new SqlConnection(conn))
+            {
+                string sql = $"select * from LMS_Client l join LMS_Ding d on l.SName=d.DName join LMS_DState e on d.DSzt=e.Id where d.dName='{name}'";
+                var list = db.Query<Client>(sql).ToList();
+
+                if (PageCurrent < 1)
+                {
+                    PageCurrent = 1;
+                }
+                int count = list.Count();
+                int index = 0;
+                if (count % PageSize == 0)
+                {
+                    index = count / PageSize;
+                }
+                else
+                {
+                    index = count / PageSize + 1;
+                }
+                if (PageCurrent > index)
+                {
+                    PageCurrent = index;
+                }
+                Models.Hwc.Page p = new Models.Hwc.Page();
+                p.List1 = list.Skip((PageCurrent - 1) * PageSize).Take(PageSize).ToList();
+                p.PageCurrent = PageCurrent;
+                p.PageCount = count;
+                p.PageIndex = index;
+                return p;
+            }
+        }
+        /// <summary>
+        /// 还款计划
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("HuanKuan")]
+        public List<RepaymentSchedule> HuanKuan(int id)
+        {
+            using (IDbConnection db = new SqlConnection(conn))
+            {
+                string sql = $"select * from RepaymentSchedule where Dingwai={id}";
+                return db.Query<RepaymentSchedule>(sql).ToList();
+            }
+        }
+
+        /// <summary>
+        /// 结清返填
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("JieQing")]
+        public RepaymentSchedule JieQing(int id)
+        {
+
+            using (IDbConnection db = new SqlConnection(conn))
+            {
+                string sql = $"select * from RepaymentSchedule where Id={id}";
+                return db.Query<RepaymentSchedule>(sql).FirstOrDefault();
+            }
+        }
+
+        /// <summary>
+        /// 结清
+        /// </summary>
+        /// <returns></returns>
+        [HttpPut]
+        [Route("JieCaoZuo")]
+        public int JieCaoZuo(int id)
+        {
+
+                var a = db.RepaymentSchedule.Where(s => s.DingWai == id && s.RepaymentMoney == 0).FirstOrDefault();
+                var b = db.LMS_Ding.Where(s => s.DId == id).FirstOrDefault();
+                if (a.Periods == b.DCount)
+                {
+                    db.Entry(b).State = EntityState.Modified;
+                    db.Entry(a).State = EntityState.Modified;
+                    b.DSzt = 13;
+                    a.RepaymentMoney = 1;
+                    return db.SaveChanges();
+                }
+                else
+                {
+                    db.Entry(a).State = EntityState.Modified;
+                    a.RepaymentMoney = 1;
+                    return db.SaveChanges();
+                }
+
+            }
+
+        /// <summary>
+        /// 绑定状态
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("LMS_DState")]
+        public List<LMS_DState> LMS_DState()
+        {
+            using (IDbConnection db = new SqlConnection(conn))
+            {
+                string sql = $"select * from LMS_DState";
+                var list = db.Query<LMS_DState>(sql).ToList();
+                return list;
+            }
+        }
 
 
     }
